3a83ff0af389148eab2507a7b7c29c2e1e9c0a14
Nimit Bhardwaj <nimitbhardwaj@gmail.com>
2018-11-03 01:40:03 +0530

    Adding login and signup activity

--- app/src/main/java/com/poseidon/nimitbhardwaj/clubsandpeople/MainActivity.java
+++ app/src/main/java/com/poseidon/nimitbhardwaj/clubsandpeople/MainActivity.java
@@ -1,13 +1,15 @@
 package com.poseidon.nimitbhardwaj.clubsandpeople;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Intent myIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myIntent = getIntent();
     }
 }
--- app/src/main/java/com/poseidon/nimitbhardwaj/clubsandpeople/SplashActivity.java
+++ app/src/main/java/com/poseidon/nimitbhardwaj/clubsandpeople/SplashActivity.java
@@ -1,13 +1,37 @@
 package com.poseidon.nimitbhardwaj.clubsandpeople;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
 
+public class SplashActivity extends AppCompatActivity {
+    private long SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        /*
+         * The Splash Screen Activity, do the work of loading the
+         * necessary data, such as the previous signed in data state
+         */
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
+        final Intent splshIntent = new Intent(this,
+                com.poseidon.nimitbhardwaj.clubsandpeople.MainActivity.class);
+
+        // The Main Work for loading the data
+        Handler handSplash = new Handler();
+        handSplash.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Bundle bundl = new Bundle();
+                captureDefaults(bundl);
+                splshIntent.putExtra("loginBundle", bundl);
+                startActivity(splshIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+    private void captureDefaults(Bundle bundl) {
     }
 }
